4b1126113b6fad17ec4bb40cd13c14a4cb70b309
Nab <35942227+Dilgithub@users.noreply.github.com>
2019-07-05 00:30:01 +0530

    featured

--- Components/Components/Featured.js
+++ Components/Components/Featured.js
@@ -1,29 +1,124 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
+import { Text, View, FlatList, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 
 class Featured extends React.Component {
   state={
-    renderItems: [{
-      v1:'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
-      v2:'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      v3:'https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
-      v4:'https://cdn.pixabay.com/photo/2018/05/28/22/11/message-in-a-bottle-3437294__340.jpg',
-      v5:'https://static.toiimg.com/photo/msid-67868104/67868104.jpg?1368689'
-    }]
+    renderItems: [
+      {uri:'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500', name: 'Aquaman'},
+      {uri:'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', name: 'Aquaman'},
+      {uri:'https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', name: 'Aquaman'},
+      {uri:'https://cdn.pixabay.com/photo/2018/05/28/22/11/message-in-a-bottle-3437294__340.jpg', name: 'Aquaman'},
+      {uri:'https://static.toiimg.com/photo/msid-67868104/67868104.jpg?1368689', name: 'Aquaman'}
+    ]
   }
     render() {
+      console.log("state", this.state)
+      let images = this.state.renderItems.map(item => {
+      console.log("Item", item.name);
+        return (
+        
+          <View style={{padding:10, marginTop:10}}>
+            <TouchableOpacity>
+              <Image style={{width: 250, height: 170, borderRadius:10}} source={{uri: item.uri}} />
+            </TouchableOpacity>
+          </View>
+        
+        )
+      })
+
+      let images1 = this.state.renderItems.map(item => {
+        console.log("Item", item.name);
+          return (
+          
+            <View style={{padding:10}}>
+              <TouchableOpacity>
+                <Image style={{width: 180, height: 100, borderRadius:10}} source={{uri: item.uri}} />
+              </TouchableOpacity>
+              {/* <Text style={{color:'white'}}>{item.name}</Text> */}
+            </View>
+          
+          )
+        })
+
+        let images2 = this.state.renderItems.map(item => {
+          console.log("Item", item.name);
+            return (
+            
+              <View style={{padding:10}}>
+                <TouchableOpacity>
+                  <Image style={{width: 120, height: 150, borderRadius:10}} source={{uri: item.uri}} />
+                </TouchableOpacity>
+                <Text style={{color:'white'}}>{item.name}</Text>
+              </View>
+            
+            )
+          })
+
+          let images3 = this.state.renderItems.map(item => {
+            console.log("Item", item.name);
+              return (
+              
+                <View style={{padding:10}}>
+                  <TouchableOpacity>
+                    <Image style={{width: 120, height: 150, borderRadius:10}} source={{uri: item.uri}} />
+                  </TouchableOpacity>
+                  <Text style={{color:'white'}}>{item.name}</Text>
+                </View>
+              
+              )
+            })
+
+            let images4 = this.state.renderItems.map(item => {
+              console.log("Item", item.name);
+                return (
+                
+                  <View style={{padding:10}}>
+                    <TouchableOpacity>
+                      <Image style={{width: 120, height: 150, borderRadius:10}} source={{uri: item.uri}} />
+                    </TouchableOpacity>
+                    <Text style={{color:'white'}}>{item.name}</Text>
+                  </View>
+                
+                )
+              })
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'black'}}>
-          {/* <Text style={{color:'white'}}>Featured!</Text> */}
-          <FlatList>
-              {/* // data={this.state.renderItems} 
-              // renderItem={(item) => { */}
-                <Image style={{width: 200, height: 200}} source={{uri: 'https://static.toiimg.com/photo/msid-67868104/67868104.jpg?1368689'}} />
-              {/* }
-            } /> */}
-            </FlatList>
-        </View>
+        <ScrollView>
+
+          <View style={{ flex: 1, flexDirection:'column', alignItems: 'flex-start', backgroundColor:'black'}}>              
+            <ScrollView horizontal>            
+                {images}
+            </ScrollView>
+          </View>
+
+
+          <View style={{flex: 1, flexDirection:'column', alignItems: 'flex-start', backgroundColor:'black'}}>
+            <Text >Continue Watching</Text> 
+            <ScrollView horizontal>
+                  {images1}
+            </ScrollView>
+          </View>
+
+          <View style={{ flex: 1, flexDirection:'row', alignItems: 'flex-start', backgroundColor:'black'}}>
+            <ScrollView horizontal>
+                {images2}
+            </ScrollView>
+          </View>
+
+          <View style={{ flex: 1, flexDirection:'row', alignItems: 'flex-start', backgroundColor:'black'}}>
+            <ScrollView horizontal>
+                {images3}
+            </ScrollView>
+          </View>
+
+          <View style={{ flex: 1, flexDirection:'row', alignItems: 'flex-start', backgroundColor:'black'}}>
+            <ScrollView horizontal>
+                {images4}
+            </ScrollView>
+          </View>
+
+        </ScrollView>
+        
       );
     }
   }
